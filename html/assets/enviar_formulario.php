ffdb7d1f1e99f3f108b0c0909c9f1b051f9b90bf
florsepulvedadv <florencia.sepulveda@davinci.edu.ar>
2018-08-31 14:19:48 -0300

    contacto

--- html/assets/enviar_formulario.php
+++ html/assets/enviar_formulario.php
@@ -1,46 +1,45 @@
 <?php
-	$emailPara	= 'hola@brontosaurio.com.ar'; 
-	$subject	= 'Contacto brontosaurio.com.ar';
 
-	$enviado	="../index.html";
-	$mensaje 	= '';
-	$primero 	= true;
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	 
+        #Reemplazar este correo por el correo electrónico del destinatario
+        $mail_to = 'hola@brontosaurio.com.ar'; 
+        $subject = 'Contacto desde Bronto web.';
+        
+        # Envío de datos
+        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["nombre"])));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["mensaje"]);
 
-	foreach($_POST as $indice => $valor){
-		if (is_array($valor)) {
-			$mensaje .= '<strong>'.$indice.': </strong><ul>';
-			
-			foreach($valor as $item) {
-				$mensaje .= '<li>'.$item .'</li>';
-			}
-
-			$mensaje .= '</ul>';
-		} else {
-			if($primero) {
-				$from = $valor;
-				$primero = false;
-			}
-
-			$mensaje .= '<strong>' . $indice . ': </strong>';
-			$mensaje .= $valor . '<br>';
-			
-			if(strpos($valor, '@') > 3 && strpos($valor, '.') > -1){
-				$from = $valor;
-			}
-		}
-	}
-
-	$mail_headers  = "MIME-Version: 1.0\r\n";
-	$mail_headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-	$mail_headers .= 'Email de: ' . $from . "\r\n";
-	
-	$send_mail 	= mail($emailPara, $subject, $mensaje, $mail_headers);
-	
-	if(!$send_mail) {
-	    /*json_encode(array('type'=>'error', 'text' => 'No se pudo enviar el mensaje'));*/
-	    echo "No se manda";
-	} else {
-	    /*json_encode(array('type'=>'message', 'text' => 'Gracias por tu mensaje ¡Te responderemos a la brevedad!'));*/
-	    echo "Se manda";
-	}
-?
+        /*if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($message)) {
+            # Establecer un código de respuesta y salida.
+            http_response_code(400);
+            echo "Por favor completa los datos :)";
+            exit;
+        }*/
+        
+        # Contenido del correo
+        $content = "Nombre: $name\n";
+        $content .= "E-mail: $email\n\n";
+        $content .= "Mensaje:\n$message\n";
+ 
+        # Encabezados de correo electrónico.
+        $headers = "From: $name <$email>";
+ 
+        # Envía el correo.
+        $success = mail($mail_to, $subject, $content, $headers);
+        if ($success) {
+            # Establece un código de respuesta 200 (correcto).
+            //http_response_code(200);
+            echo 'Gracias por tu mensaje ¡Te responderemos a la brevedad!';
+        } else {
+            # Establezce un código de respuesta 500 (error interno del servidor).
+            //http_response_code(500);
+            echo 'Uy! Algo salió mal, no pudimos enviar tu mensaje.';
+        }
+    } else {
+        # No es una solicitud POST, establezce un código de respuesta 403 (prohibido).
+        //http_response_code(403);
+    	echo 'Hubo un problema con tu envío, intenta de nuevo.';
+    }
+?>
